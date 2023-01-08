2fca39feb8ea25960b7f85ecb5dd99bdb554aaed
dukjjang <jhd7292@naver.com>
2023-01-09 06:19:55 +0900

    design: 모바일 height 수정

--- src/components/CardDetail/index.tsx
+++ src/components/CardDetail/index.tsx
@@ -104,7 +104,7 @@ const CardDetail = ({
 
   return (
     <div className=' fixed top-0 left-0 w-full z-20 h-screen '>
-      <div className='  flex justify-center items-center bg-[rgba(0,0,0,0.03)]  h-screen'>
+      <div className='  flex justify-center items-center bg-[rgba(0,0,0,0.03)]  h-full'>
         <form
           ref={domNode as LegacyRef<HTMLFormElement>}
           onSubmit={handleSubmit}
