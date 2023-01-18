24e0baf41f3c189f4cc92e2494a07cb32aae0596
dukjjang <jhd7292@naver.com>
2023-01-18 17:26:18 +0900

    design: 날짜 css 추가

--- src/components/Today/index.tsx
+++ src/components/Today/index.tsx
@@ -4,10 +4,12 @@ const Today = () => {
   const { date, month, year } = getCurrentDate();
   return (
     <div className="text-sm lg:text-3xl flex flex-col">
-      <h1 className=" text-2xl lg:text-6xl text-center">{date}</h1>
+      <h1 className=" text-red-800 font-bold text-2xl lg:text-6xl text-center">
+        {date}
+      </h1>
       <div className="flex items-center">
-        <h1 className="mr-1">{month}</h1>
-        <h1>{year}</h1>
+        <h1 className="mr-1 text-slate-300 ">{month}</h1>
+        <h1 className="text-green-800">{year}</h1>
       </div>
     </div>
   );
